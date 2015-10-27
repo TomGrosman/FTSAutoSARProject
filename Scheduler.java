2ccd1cb4bef8e566a63ef91e1c8c41bb2c0997e9
TomGrosman <tom@grosman.org>
2015-10-27 23:55:20 +0100

    Danesh's version

--- Scheduler.java
+++ Scheduler.java
@@ -1,18 +1,27 @@
-/**
- * @(#) Scheduler.java
- * Fault Tolerant Systems AutoSAR project
- * 
- * Created by trg on Oct 21, 2015
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Scheduler
-{
-        private ECU scheduler;
-        
-        public Scheduler( )
-        {
-                
-        }
-        
-        
+
+public class Scheduler {
+
+	public List<OsTask> tasks;
+	
+	public Scheduler() {
+		this.tasks = new ArrayList<OsTask>();
+	}
+
+	public void addOsTask(OsTask _osTask) {
+		this.tasks.add(_osTask);
+	}
+	
+	public void start() {
+		for (int i=0; i<tasks.size(); i++) {
+			tasks.get(i).state = OsTaskStateEnum.READY;				
+		}
+		Collections.sort(tasks);
+		String a = "";
+	}
+	
+	
 }
