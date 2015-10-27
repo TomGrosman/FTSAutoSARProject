5fcce7fc5d5efb21aad87b26043e4770e80db509
TomGrosman <tom@grosman.org>
2015-10-27 23:51:22 +0100

    Update OSTask.java
    Danesh's version

--- OSTask.java
+++ OSTask.java
@@ -1,23 +1,33 @@
-/**
- * @(#) Task.java
- * Fault Tolerant Systems AutoSAR project
- * 
- * Created by trg on Oct 21, 2015
- */
 
-public class Task
-{
-        private SoftwareComponent softwarecomponent;
-        
-        private Runnable runnable;
-        
-        /**
-         * @param softwarecomponent
-         */
-        public Task( SoftwareComponent softwarecomponent )
-        {
-                
-        }
-        
-        
+public class OsTask implements Comparable<OsTask> {
+
+	public Runnable runnable;
+	public OsTaskStateEnum state;
+	
+	public double wcet;
+	public double period;
+	
+	public double priority;
+	
+	public OsTask(Runnable _runnable) {
+		this.runnable = _runnable;
+	}
+
+	public OsTask(double _wcet, double _period) {
+		this.wcet = _wcet;
+		this.period = _period;
+		this.priority = 1/_period;
+		this.state = OsTaskStateEnum.WAITING;
+	}
+
+	@Override
+	public int compareTo(OsTask _task) {
+
+		int stateInfluenceThis = this.state == OsTaskStateEnum.READY ? 1 : 0;
+		int stateInfluenceParam = _task.state == OsTaskStateEnum.READY ? 1 : 0;
+		
+		// order by period to avoid the double issue
+		return (int) (((double) ((_task.priority * stateInfluenceParam) - (this.priority * stateInfluenceThis)))*1000);
+	}
+	
 }
